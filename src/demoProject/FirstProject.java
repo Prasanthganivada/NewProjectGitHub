8dfe3d0f83a604a524fabe0fa4820c9b3bd75799
Prasanth <Prasanth@DESKTOP-LA9THFC>
2022-02-23 10:46:53 +0530

    First Commit to the Project...

---
+++ src/demoProject/FirstProject.java
@@ -0,0 +1,10 @@
+package demoProject;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Team...");
+		System.out.println("This is our First Project On GITHUB");
+	}
+
+}
